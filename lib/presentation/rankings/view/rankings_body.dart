89b50ddb80a4fe1011904dd868bb268ab7405c61
Verzeri, C., ext., SNL IT P&P, BN, external <carlo.verzeri@deutschepost.de>
2024-06-25 16:34:01 +0200

    styles rankings body

--- lib/presentation/rankings/view/rankings_body.dart
+++ lib/presentation/rankings/view/rankings_body.dart
@@ -2,6 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:vote_circle_repository/vote_circle_repository.dart';
 import 'package:vote_your_face/presentation/routes/router.gr.dart';
+import 'package:vote_your_face/presentation/shared/shared.dart';
+import 'package:vote_your_face/theme.dart';
 
 class RankingsBody extends StatelessWidget {
   const RankingsBody({
@@ -20,7 +22,8 @@ class RankingsBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
             child: Text(
               'Last viewed',
               style: themeData.textTheme.titleMedium,
@@ -45,28 +48,89 @@ class RankingsBody extends StatelessWidget {
       itemBuilder: (BuildContext contextListView, int index) {
         final circle = circles[index];
 
-        return Card(
-          key: Key(circle.id.toString()),
-          elevation: 0,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.zero,
-          ),
-          margin: const EdgeInsets.all(0),
-          child: ListTile(
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 15.0,
-              vertical: 3.0,
+        return GestureDetector(
+          onTap: () => context.router.push(RankingRoute(circleId: circle.id)),
+          child: Card(
+            key: Key(circle.id.toString()),
+            elevation: 1,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero,
             ),
-            title: Text(
-              circle.name,
-              style: themeData.textTheme.titleMedium,
+            margin: const EdgeInsets.all(0),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 32,
+                    width: 32,
+                    child: NetImage(
+                      imageSrc: circle.imageSrc,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          circle.name,
+                          style: themeData.textTheme.titleMedium,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                        Text(
+                          circle.description,
+                          style: themeData.textTheme.bodySmall,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(Icons.arrow_forward_ios_outlined),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: themeData.textTheme.labelSmall,
+                            children: [
+                              const TextSpan(
+                                text: 'Active:',
+                              ),
+                              WidgetSpan(
+                                child: Icon(
+                                  Icons.circle_rounded,
+                                  size: 10,
+                                  color: circle.stage == CircleStage.hot
+                                      ? themeData.colorScheme.successColor
+                                      : themeData.colorScheme.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
-            onTap: () => context.router.push(RankingRoute(circleId: circle.id)),
           ),
         );
       },
-      separatorBuilder: (context, index) => const SizedBox(height: 10),
+      separatorBuilder: (context, index) => const SizedBox(
+        height: 5,
+      ),
     );
   }
 }
