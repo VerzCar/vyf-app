ba410a7594caf53cf4d02f505801fe7ec0fbd695
verzcar <carlo.verzeri@vecomentman.de>
2024-08-09 16:03:21 +0200

    added candidate search part 2

--- lib/presentation/members/widgets/candidate_search.dart
+++ lib/presentation/members/widgets/candidate_search.dart
@@ -15,118 +15,116 @@ class CandidateSearch extends StatelessWidget {
     late FocusNode searchFocusNode;
 
     return Autocomplete<UserPaginated>(
-      displayStringForOption: (user) => user.username,
-      optionsBuilder: (TextEditingValue textEditingValue) async {
-        try {
-          final userRepo = sl<IUserRepository>();
-          final Iterable<UserPaginated> users =
-              await userRepo.usersFiltered(textEditingValue.text);
+        displayStringForOption: (user) => user.username,
+        optionsBuilder: (TextEditingValue textEditingValue) async {
+          try {
+            final userRepo = sl<IUserRepository>();
+            final Iterable<UserPaginated> users =
+                await userRepo.usersFiltered(textEditingValue.text);
 
-          return users.isEmpty ? [UserPaginated.empty] : users;
-        } catch (e) {
-          print(e);
-          return [UserPaginated.empty];
-        }
-      },
-      optionsViewBuilder: (
-        BuildContext context,
-        AutocompleteOnSelected<UserPaginated> onSelected,
-        Iterable<UserPaginated> options,
-      ) {
-        if (options.isEmpty || options.length == 1 && options.first.id == 0) {
-          return const Scaffold(
-            body: Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('No Results'),
-                ],
+            return users.isEmpty ? [UserPaginated.empty] : users;
+          } catch (e) {
+            print(e);
+            return [UserPaginated.empty];
+          }
+        },
+        optionsViewBuilder: (
+          BuildContext context,
+          AutocompleteOnSelected<UserPaginated> onSelected,
+          Iterable<UserPaginated> options,
+        ) {
+          if (options.isEmpty || options.length == 1 && options.first.id == 0) {
+            return const Scaffold(
+              body: Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('No Results'),
+                  ],
+                ),
+              ),
+            );
+          }
+
+          final themeData = Theme.of(context);
+          final size = MediaQuery.of(context).size;
+
+          return Scaffold(
+            resizeToAvoidBottomInset: true,
+            body: ListView.separated(
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 10),
+              itemCount: options.length,
+              itemBuilder: (BuildContext context, int index) {
+                final user = options.elementAt(index);
+
+                return Card(
+                  key: Key(user.id.toString()),
+                  elevation: 0,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  margin: const EdgeInsets.all(0),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 15.0,
+                    ),
+                    onTap: () => onSelected(user),
+                    leading: AvatarImage(
+                      src: user.profile.imageSrc,
+                      capitalLetters: usersInitials(user.displayName),
+                    ),
+                    title: Text(user.displayName),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => Divider(
+                height: 3,
+                thickness: 3,
+                color: themeData.colorScheme.blackColor,
               ),
             ),
           );
-        }
-
-        final themeData = Theme.of(context);
-        final size = MediaQuery.of(context).size;
-
-        return SizedBox(
-          //height: constraints.biggest.height,
-          child: ListView.separated(
-            padding: const EdgeInsets.only(top: 10),
-            itemCount: options.length,
-            itemBuilder: (BuildContext context, int index) {
-              final user = options.elementAt(index);
-
-              return Card(
-                key: Key(user.id.toString()),
-                elevation: 0,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero,
-                ),
-                margin: const EdgeInsets.all(0),
-                child: ListTile(
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 15.0,
+        },
+        onSelected: (UserPaginated selection) {
+          context.read<CandidatesSelectCubit>().addToSelection(user: selection);
+          searchController.clear();
+          searchFocusNode.unfocus();
+        },
+        fieldViewBuilder: (
+          BuildContext context,
+          TextEditingController controller,
+          FocusNode focusNode,
+          VoidCallback onFieldSubmitted,
+        ) {
+          searchController = controller;
+          searchFocusNode = focusNode;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Search candidate',
+                    ),
+                    focusNode: focusNode,
                   ),
-                  onTap: () => onSelected(user),
-                  leading: AvatarImage(
-                    src: user.profile.imageSrc,
-                    capitalLetters: usersInitials(user.displayName),
-                  ),
-                  title: Text(user.displayName),
                 ),
-              );
-            },
-            separatorBuilder: (context, index) => Divider(
-              height: 3,
-              thickness: 3,
-              color: themeData.colorScheme.blackColor,
+                const SizedBox(width: 3),
+                TextButton(
+                  onPressed: () {
+                    searchController.clear();
+                    searchFocusNode.unfocus();
+                  },
+                  child: const Text('cancel'),
+                ),
+              ],
             ),
-          ),
-        );
-      },
-      onSelected: (UserPaginated selection) {
-        context.read<CandidatesSelectCubit>().addToSelection(user: selection);
-        searchController.clear();
-        searchFocusNode.unfocus();
-      },
-      fieldViewBuilder: (
-        BuildContext context,
-        TextEditingController controller,
-        FocusNode focusNode,
-        VoidCallback onFieldSubmitted,
-      ) {
-        searchController = controller;
-        searchFocusNode = focusNode;
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: controller,
-                  decoration: const InputDecoration(
-                    hintText: 'Search candidate',
-                  ),
-                  focusNode: focusNode,
-                ),
-              ),
-              const SizedBox(width: 3),
-              focusNode.hasFocus
-                  ? TextButton(
-                      onPressed: () {
-                        searchController.clear();
-                        searchFocusNode.unfocus();
-                        focusNode.unfocus();
-                      },
-                      child: const Text('cancel'),
-                    )
-                  : const SizedBox(),
-            ],
-          ),
-        );
-      },
-    );
+          );
+        },
+      );
   }
 }
